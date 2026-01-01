468c2072c489d381d0df65b70de3445dddd1227e
igmalyshev <ilia.malishev@gmail.com>
2026-01-02 01:22:58 +0300

    Increase version

--- src/habot/__init__.py
+++ src/habot/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 __author__ = 'Ilya Malyshev'
 __email__ = 'ilia.malishev@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 from .bot import HaBot
